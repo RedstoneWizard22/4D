270ec61ec660a186e6fe839642f9ca6322d2a6a8
RedstoneWizard22 <redstone@rwizard22.com>
2022-08-15 15:59:30 +0100

    ✨ Basic error handling in AnimatedScene

--- src/ui/components/AnimatedScene.svelte
+++ src/ui/components/AnimatedScene.svelte
@@ -4,6 +4,11 @@
   import { fade } from 'svelte/transition';
   import { useAnimationLoop } from '../utilities/use-animation-loop';
 
+  type ErrorDetails = {
+    title: string;
+    message: string;
+  };
+
   export let callbacks: {
     init: (canvas: HTMLCanvasElement, width: number, height: number) => void;
     frame: (delta: number) => void;
@@ -17,6 +22,7 @@
   let canvas: HTMLCanvasElement;
   let animationLoop = useAnimationLoop({ autoStart: false });
   let initialized = false;
+  let error: ErrorDetails | undefined;
 
   $: animationLoop.setOptions({ maxFps });
 
@@ -28,21 +34,50 @@
     }
   }
 
-  onMount(() => {
-    init();
+  onMount(init);
+
+  function init() {
+    try {
+      callbacks.init(canvas, parent.clientWidth, parent.clientHeight);
+    } catch (e) {
+      error = {
+        title: 'Error initializing scene',
+        message: 'The scene failed to initialize. Please reload the page to try again.',
+      };
+      destroy();
+      console.error(e);
+      return;
+    }
+
     window.addEventListener('resize', resize, false);
+    canvas.addEventListener(
+      'webglcontextlost',
+      () => {
+        error = {
+          title: 'WebGL context lost',
+          message: 'Your browser lost the WebGL context. Please reload the page.',
+        };
+        destroy();
+      },
+      false
+    );
     animationLoop.add(frame);
     initialized = true;
 
     return destroy;
-  });
-
-  function init() {
-    callbacks.init(canvas, parent.clientWidth, parent.clientHeight);
   }
 
   function frame(delta: number) {
-    callbacks.frame(delta);
+    try {
+      callbacks.frame(delta);
+    } catch (e) {
+      error = {
+        title: 'Error rendering frame',
+        message: 'An error occured during rendering. Please reload the page to try again.',
+      };
+      destroy();
+      console.error(e);
+    }
   }
 
   function resize() {
@@ -50,6 +85,7 @@
   }
 
   function destroy() {
+    callbacks.destroy(canvas);
     animationLoop.remove(frame);
     animationLoop.stop();
     window.removeEventListener('resize', resize, false);
@@ -58,12 +94,18 @@
 
 <div class="relative h-full w-full" bind:this={parent}>
   <canvas class="h-full w-full" bind:this={canvas} />
-  {#if loading || !initialized}
+  {#if !error && (loading || !initialized)}
     <div class="absolute top-0 left-0 z-10 h-full w-full bg-gray-900 bg-opacity-10" transition:fade>
       <!-- loader from https://codepen.io/supah/pen/BjYLdW -->
       <svg class="spinner" viewBox="0 0 50 50">
         <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5" />
       </svg>
+    </div>
+  {/if}
+  {#if error}
+    <div class="absolute top-0 left-0 z-10 h-full w-full bg-white">
+      <p>{error.title}</p>
+      <p>{error.message}</p>
     </div>
   {/if}
 </div>
