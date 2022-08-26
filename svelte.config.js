ae3f0c777d3ce57b0b481afceaf16a5982e90906
RedstoneWizard22 <redstone@rwizard22.com>
2022-08-26 09:30:53 +0100

    🔧 Configure build settings

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -18,6 +18,9 @@ const config = {
 
   kit: {
     adapter: adapter(),
+    prerender: {
+      default: true,
+    },
   },
 };
 
