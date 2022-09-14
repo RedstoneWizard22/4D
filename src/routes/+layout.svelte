d0480476a1c9b32bc69b6a248dedc0418d83e6a8
RedstoneWizard22 <redstone@rwizard22.com>
2022-09-14 19:00:58 +0100

    💚 Fix build issues
    I need to remember to check if the app builds before pushing :p

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,5 @@
 <script>
   import '../app.postcss';
-
-  // Enable prerendering for the whole site
-  export const csr = false;
-  export const prerender = true;
 </script>
 
 <slot />
---
+++ src/routes/+layout.ts
@@ -0,0 +1,3 @@
+// Enable prerendering for the whole site
+export const csr = false;
+export const prerender = true;
