e95b3d1f6a6787169bb2c9920f0a4d3387fb1384
RedstoneWizard22 <redstone@rwizard22.com>
2022-09-14 18:24:11 +0100

    ➖ Remove mdsvex
    It doesn't play well with typescript so easier not to use it

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,9 @@
-import { mdsvex } from 'mdsvex';
-import mdsvexConfig from './mdsvex.config.js';
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  extensions: ['.svelte', ...mdsvexConfig.extensions],
+  extensions: ['.svelte'],
 
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
@@ -13,7 +11,6 @@ const config = {
     preprocess({
       postcss: true,
     }),
-    mdsvex(mdsvexConfig),
   ],
 
   kit: {
